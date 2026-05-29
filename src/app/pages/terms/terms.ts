43ca73460f356c84a355cb5ce139c82965f88504
Mohit Kumar <yourmail@example.com>
2026-05-29 15:50:09 +0530

    connected terms page buttons with navigation

--- src/app/pages/terms/terms.ts
+++ src/app/pages/terms/terms.ts
@@ -1,9 +1,28 @@
 import { Component } from '@angular/core';
 
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-terms',
   imports: [],
   templateUrl: './terms.html',
-  styleUrl: './terms.css',
+  styleUrl: './terms.css'
 })
-export class Terms {}
+
+export class Terms {
+
+  constructor(private router: Router) {}
+
+  acceptTerms() {
+
+    this.router.navigate(['/personal']);
+
+  }
+
+  declineTerms() {
+
+    this.router.navigate(['/']);
+
+  }
+
+}
