d3df282130ee924635e7fdbe5d7667326516f6aa
Mohit Kumar <yourmail@example.com>
2026-05-29 14:53:48 +0530

    convert register page into angular component with validation

--- src/app/pages/register/register.ts
+++ src/app/pages/register/register.ts
@@ -6,4 +6,77 @@ import { Component } from '@angular/core';
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
-export class Register {}
+
+export class Register {
+
+  sendCode() {
+
+    const mobile =
+    document.getElementById("mobile") as HTMLInputElement;
+
+    const mobileError =
+    document.getElementById("mobileError") as HTMLElement;
+
+    if(mobile.value.trim() === ""){
+
+      mobile.style.border =
+      "2px solid red";
+
+      mobileError.style.display =
+      "block";
+
+    }
+
+    else{
+
+      mobile.style.border =
+      "2px solid #ccc";
+
+      mobileError.style.display =
+      "none";
+
+      window.location.href =
+      "/verify";
+
+    }
+
+  }
+
+  checkMobile() {
+
+    const mobile =
+    document.getElementById("mobile") as HTMLInputElement;
+
+    const mobileError =
+    document.getElementById("mobileError") as HTMLElement;
+
+    if(mobile.value.trim() === ""){
+
+      mobile.style.border =
+      "2px solid red";
+
+      mobileError.style.display =
+      "block";
+
+    }
+
+    else{
+
+      mobile.style.border =
+      "2px solid #ccc";
+
+      mobileError.style.display =
+      "none";
+
+    }
+
+  }
+
+  onlyNumbers(event: any) {
+
+    event.target.value =
+    event.target.value.replace(/[^0-9]/g, "");
+
+  }
+
+}
