040e7eec04f17a17880c281715c992bbd3ac78f9
Mohit Kumar <yourmail@example.com>
2026-05-29 16:53:21 +0530

    add adress files

---
+++ src/app/pages/address/address.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-address',
+  imports: [],
+  templateUrl: './address.html',
+  styleUrl: './address.css',
+})
+export class Address {}
