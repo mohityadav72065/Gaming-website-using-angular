bb540d2e9807af63bcdbfdaa49e7e1b33137fe03
Mohit Kumar <yourmail@example.com>
2026-05-29 15:17:36 +0530

    added verify page html and ts functionality

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,39 +1,35 @@
 import { Routes } from '@angular/router';
 
 import { Login } from './pages/login/login';
-
 import { Register } from './pages/register/register';
-
 import { Verify } from './pages/verify/verify';
-
 import { Terms } from './pages/terms/terms';
-
 import { Personal } from './pages/personal/personal';
 
 export const routes: Routes = [
 
   {
-    path:'',
+    path: '',
     component: Login
   },
 
   {
-    path:'register',
+    path: 'register',
     component: Register
   },
 
   {
-    path:'verify',
+    path: 'verify',
     component: Verify
   },
 
   {
-    path:'terms',
+    path: 'terms',
     component: Terms
   },
 
   {
-    path:'personal',
+    path: 'personal',
     component: Personal
   }
 
--- src/app/pages/verify/verify.ts
+++ src/app/pages/verify/verify.ts
@@ -1,9 +1,69 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-verify',
   imports: [],
   templateUrl: './verify.html',
-  styleUrl: './verify.css',
+  styleUrl: './verify.css'
 })
-export class Verify {}
+
+export class Verify{
+
+  constructor(private router: Router) {}
+
+  resendCode() {
+
+    alert('Verification code resent successfully');
+
+  }
+
+  verifyCode() {
+
+    const code =
+    document.getElementById('code') as HTMLInputElement;
+
+    if(code.value.trim() === '') {
+
+      code.style.border =
+      '2px solid red';
+
+      alert('Please fill this field to continue');
+
+    }
+
+    else if(code.value !== '180901') {
+
+      code.style.border =
+      '2px solid red';
+
+      alert('Invalid Verification Code');
+
+    }
+
+    else {
+
+      code.style.border =
+      '2px solid #ccc';
+
+      alert('Verification Successful');
+
+      this.router.navigate(['/terms']);
+
+    }
+
+  }
+
+  clearCode() {
+
+    const code =
+    document.getElementById('code') as HTMLInputElement;
+
+    code.value = '';
+
+    code.style.border =
+    '2px solid #ccc';
+
+  }
+
+}
